3b8492810cbf51e1dcb132d523065478bcee1ddf
GoItTutor <sheva09a@gmail.com>
2023-07-25 20:52:43 +0200

    module-08-lesson-01

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
 console.log('Hello from index.js');
+
+console.log(5 + 10);
+
+const arrowFunc = (a, b) => a + b;
+
+const arr = [1, 2, 4];
+
+const [first, , third] = arr;
+
+console.log(first, third);
